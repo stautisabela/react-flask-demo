3c66044c52f943534b9e8ac0589303b145fe5cd0
stautisabela <stautisabela@gmail.com>
2023-11-01 12:47:12 +0100

    connected to db

--- app.py
+++ app.py
@@ -1,31 +1,43 @@
 from flask import Flask, render_template, request, redirect, url_for
 from forms import Todo
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY']=''
+app.config['SECRET_KEY'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/test.db'
+db = SQLAlchemy(app)
+
+class TodoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(240))
+    
+    def __str__(self):
+        return f'{self.content}, {self.id}'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     request_method = request.method
+    todo = TodoModel.query.all()
     if request.method == 'POST':
         first_name = request.form['first_name']
         return redirect(url_for('name', first_name=first_name))
-    return render_template('index.html', request_method=request_method)
+    return render_template('index.html', request_method=request_method, todo=todo)
 
 
 @app.route('/todo', methods=['GET', 'POST'])
 def todo():
     todo_form = Todo()
     if todo_form.validate_on_submit():
-        print(todo_form.content.data)
+        todo = TodoModel(content=todo_form.content.data)
+        db.session.add(todo)
+        db.session.commit()
         return redirect('/')
     return render_template('todo.html', form=todo_form)
 
 
-@app.route('/name/<first_name>')
-def name(first_name):
-    return f'Hello, {first_name}'
+with app.app_context():
+    db.create_all()
 
 
 if __name__ == '__main__':
