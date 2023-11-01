fed687704523fe7540f89e726633d6ce92f5637e
stautisabela <stautisabela@gmail.com>
2023-11-01 10:48:21 +0100

    added POST method

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
+from forms import Todo
+
 app = Flask(__name__)
+app.config['SECRET_KEY']=''
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -7,11 +11,22 @@ def index():
     if request.method == 'POST':
         first_name = request.form['first_name']
         return redirect(url_for('name', first_name=first_name))
-    return render_template('index.html', request_method = request_method)
+    return render_template('index.html', request_method=request_method)
 
-@app.route('/<first_name>')
+
+@app.route('/todo', methods=['GET', 'POST'])
+def todo():
+    todo_form = Todo()
+    if todo_form.validate_on_submit():
+        print(todo_form.content.data)
+        return redirect('/')
+    return render_template('todo.html', form=todo_form)
+
+
+@app.route('/name/<first_name>')
 def name(first_name):
-    return 'Hello, {first_name}'
+    return f'Hello, {first_name}'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
