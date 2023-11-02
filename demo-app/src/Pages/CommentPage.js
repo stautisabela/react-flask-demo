c10207e0524b610983134fb9c4075adfae1816af
stautisabela <stautisabela@gmail.com>
2023-11-02 22:39:46 +0100

    implemented automatic page refresh

--- demo-app/src/Pages/CommentPage.js
+++ demo-app/src/Pages/CommentPage.js
@@ -29,7 +29,19 @@ export const CommentPage = ()=> {
                 "Content-type": "application/json; charset=UTF-8"
             }
         }).then(response => response.json())
-          .then(message => console.log(message))
+          .then(message => {
+                console.log(message)
+                setAddComment('')
+                getLatestComments()
+          })
+    }
+
+    const getLatestComments = () => {
+        fetch('/api').then(response => {
+            if(response.ok) {
+                return response.json()
+            }
+        }).then(data => setComment(data))
     }
 
     return(
