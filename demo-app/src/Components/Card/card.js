e5e99ddcf72f2c47121e42fc49516417d5aca641
stautisabela <stautisabela@gmail.com>
2023-11-05 13:31:23 +0100

    implemented list animation

--- demo-app/src/Components/Card/card.js
+++ demo-app/src/Components/Card/card.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import {Link} from "react-router-dom"
+import {useTransition, animated} from "react-spring"
 
 export const Card = ({ listOfComments })=> {
-    return (
-    <>
-        {listOfComments.map(comment => {
-            return(
-                <ul key={comment.id}>
-                    <li>
-                        <Link to={`${comment.id}`}>{comment.content}</Link>
-                    </li>
-                </ul>
-            )
-        })}
-    </>
-    )
+
+    const transition = useTransition(listOfComments, listOfComments => listOfComments.id, {
+        from: {
+            opacity:1,
+            width: '4%',
+            marginLeft: -100,
+            marginRight: 100
+        },
+        enter:{
+            opacity: 0,
+            width: '100%',
+            padding: '5px 0',
+            marginLeft: 0,
+            marginRight: 0
+        }
+    })
+    return transition.map(({ item, key, props }) => (
+        <animated.ul key={item.id} style={props}>
+            <li>
+                <Link to={`${item.id}`}>{item.content}</Link>
+            </li>
+        </animated.ul>
+    ))
 }
